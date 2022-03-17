00902417a3cc285ec690c5473257d31203176817
doyun <doyune0707@gmail.com>
2022-03-17 10:34:41 +0900

    [feat] 프로젝트 생성

---
+++ SwiftUIDara/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDara
+//
+//  Created by 김도윤 on 2022/03/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDara/SwiftUIDaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDaraApp.swift
+//  SwiftUIDara
+//
+//  Created by 김도윤 on 2022/03/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
